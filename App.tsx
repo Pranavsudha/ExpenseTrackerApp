2e4c3fdfa5d4b6d399398c235f252e8b92ccc366
Pranavan Sudhahar <pranavan@codelantic.com>
2023-12-05 14:25:11 +0530

    changes made

--- App.tsx
+++ App.tsx
@@ -1,12 +1,84 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import AllExpenses from './app/src/screens/AllExpenses';
+import RecentExpenses from './app/src/screens/RecentExpenses';
+import ManageExpenses from './app/src/screens/ManageExpenses';
+import { Ionicons } from "@expo/vector-icons";
+import { GlobalStyles } from './constants/styles';
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function ExpensesOverview() {
+  return ( 
+    <BottomTabs.Navigator
+    screenOptions={{
+      headerStyle:{
+        backgroundColor:GlobalStyles.colors.primary500,
+      },
+      headerTintColor:'white',
+      tabBarActiveTintColor:GlobalStyles.colors.accent500,
+      tabBarStyle:{
+        backgroundColor:GlobalStyles.colors.primary500
+      }
+    }}
+    >
+      <BottomTabs.Screen
+        name="RecentExpenses"
+        component={RecentExpenses}
+        options={{
+          tabBarLabel: "User",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="hourglass" size={size} color={color} />
+          ),
+          tabBarStyle: {
+            backgroundColor:GlobalStyles.colors.primary500,
+          },
+
+        }}
+      />
+      <BottomTabs.Screen
+        name="AllExpenses"
+        component={AllExpenses}
+        options={{
+          tabBarLabel: "User",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="calendar" size={size} color={color} />
+          ),
+        }}
+      />
+    </BottomTabs.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style='auto'/>
+      <NavigationContainer>
+        <Stack.Navigator 
+        screenOptions={{
+          contentStyle: { backgroundColor:GlobalStyles.colors.primary500 },
+        }}
+        >
+        <Stack.Screen
+            name="ExpensesOverview"
+            component={ExpensesOverview}
+            options={{headerShown:false}}
+          />
+          <Stack.Screen
+            name="ManageExpenses" // Fix the typo here
+            component={ManageExpenses}
+
+          />
+         
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
